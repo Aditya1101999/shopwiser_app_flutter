4f092edc2432a69d7572604dc0c9c4ac25b57bf0
Aditya Choudhary <adi@Adityas-MacBook-Air.local>
2023-03-05 15:39:10 +0530

    FINAL COMMIT

--- lib/pages/login-page.dart
+++ lib/pages/login-page.dart
@@ -62,64 +62,66 @@ class _LoginPageState extends State<LoginPage> {
                 Padding(
                   padding: const EdgeInsets.symmetric(
                       vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                          decoration: InputDecoration(
-                            hintText: "Enter Username",
-                            labelText: "Username",
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "Username can't be empty";
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            name = value;
-                            setState(() {});
-                          }),
-                      TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            hintText: "Enter Password",
-                            labelText: "Password",
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return "Password can't be empty";
-                            } else if (value.length < 6) {
-                              return "Password must contains 6 letters";
-                            }
-                            return null;
-                          }),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      InkWell(
-                        onTap: (() => moveToHome(context)),
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          width: changeButton ? 50 : 150,
-                          height: 40,
-                          alignment: Alignment.center,
-                          child: changeButton
-                              ? Icon(Icons.done, color: Colors.white)
-                              : Text(
-                                  "Continue",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                ),
-                          decoration: BoxDecoration(
-                              // ignore: deprecated_member_use
-                              color: context.theme.buttonColor,
-                              borderRadius:
-                                  BorderRadius.circular(changeButton ? 50 : 8)),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Enter Username",
+                              labelText: "Username",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Username can't be empty";
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              name = value;
+                              setState(() {});
+                            }),
+                        TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Enter Password",
+                              labelText: "Password",
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Password can't be empty";
+                              } else if (value.length < 6) {
+                                return "Password must contains 6 letters";
+                              }
+                              return null;
+                            }),
+                        SizedBox(
+                          height: 40.0,
                         ),
-                      ),
-                    ],
+                        InkWell(
+                          onTap: (() => moveToHome(context)),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeButton ? 50 : 150,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: changeButton
+                                ? Icon(Icons.done, color: Colors.white)
+                                : Text(
+                                    "Continue",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                            decoration: BoxDecoration(
+                                // ignore: deprecated_member_use
+                                color: context.theme.buttonColor,
+                                borderRadius:
+                                    BorderRadius.circular(changeButton ? 50 : 8)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
